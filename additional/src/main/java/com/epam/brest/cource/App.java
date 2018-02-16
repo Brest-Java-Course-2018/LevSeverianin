4f12ad6ec6233b2401402d74c16e5eefdc9e78b0
Livanski <w8tosk8@gmail.com>
2018-02-16 21:03:34 +0300

    add methods db create table, add user, get users

---
+++ additional/src/main/java/com/epam/brest/cource/App.java
@@ -0,0 +1,24 @@
+package com.epam.brest.cource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws SQLException, ClassNotFoundException {
+        System.out.println( "Hello World!" );
+
+        DBUtils dbUtils = new DBUtils();
+        Connection connection = dbUtils.getConnection();
+        dbUtils.createUserTable(connection);
+        dbUtils.addUser(connection, "admin", "admin", "user admin");
+        dbUtils.addUser(connection, "admin1", "admin1", "user admin1");
+        dbUtils.addUser(connection, "admin2", "admin2", "user admin2");
+        dbUtils.getUsers(connection);
+
+    }
+}
